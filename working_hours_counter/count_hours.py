71011ec2cf18a01c08ea3aa4b332b19cbd909bc3
jmBohen <jmbochenski@gmail.com>
2024-08-20 20:06:13 +0200

    Rounding added

--- working_hours_counter/count_hours.py
+++ working_hours_counter/count_hours.py
@@ -7,13 +7,15 @@ def main():
     payment = float(input("Hour wage: "))
     with open(sys.argv[1], "r") as file:
         for line in file:
+            if line == "\n": continue
             hours += count_hours(line)
 
+    hours = round(hours,2)
     print("Sum of hours: ", hours)
-    print("Salary: ", f"{hours} * {payment} =", hours * payment, "zł")
+    print("Salary: ", f"{hours} * {payment} =", round(hours * payment,2), "zł")
 
 def count_hours(line):
-    matches = re.search(r"^\d{1,2}\D*(\d{1,2}):(\d{2})\D*(\d{1,2}):(\d{2})$", line)
+    matches = re.search(r"^.*(\d{1,2}):(\d{2})\D*(\d{1,2}):(\d{2})$", line)
     times = list()
     for _ in range(4):
         times.append(float(matches.group(_+1)))
@@ -21,7 +23,7 @@ def count_hours(line):
     time1 = hour1 + minutes1/60
     time2 = hour2 + minutes2/60
     delta = time2 - time1 
-    print(line.strip("\n"), f"   Number of hours: {delta}")
+    print(line.strip("\n"), f"   Number of hours: {round(delta,2)}")
     return delta
 
 
