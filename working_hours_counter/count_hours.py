cc70094f6f0028d4aacb411e06f981e1406f8380
jmBohen <jmbochenski@gmail.com>
2024-08-20 15:41:15 +0200

    Created hours counter

---
+++ working_hours_counter/count_hours.py
@@ -0,0 +1,35 @@
+import sys
+import re
+
+def main():
+    check_args()
+    hours = 0
+    payment = float(input("Hour wage: "))
+    with open(sys.argv[1], "r") as file:
+        for line in file:
+            hours += count_hours(line)
+
+    print("Sum of hours: ", hours)
+    print("Salary: ", f"{hours} * {payment} =", hours * payment, "zł")
+
+def count_hours(line):
+    matches = re.search(r"^\d{1,2}\D*(\d{1,2}):(\d{2})\D*(\d{1,2}):(\d{2})$", line)
+    times = list()
+    for _ in range(4):
+        times.append(float(matches.group(_+1)))
+    hour1, minutes1, hour2, minutes2 = times
+    time1 = hour1 + minutes1/60
+    time2 = hour2 + minutes2/60
+    delta = time2 - time1 
+    print(line.strip("\n"), f"   Number of hours: {delta}")
+    return delta
+
+
+def check_args():
+    if len(sys.argv) != 2:
+        sys.exit("\nPass a file name as a second argument! \n")
+    
+
+
+if __name__ == "__main__":
+    main()
