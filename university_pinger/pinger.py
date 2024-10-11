9ed563c5715356639418566aac109df8b5920d74
jmBohen <jmbochenski@gmail.com>
2024-10-11 21:23:53 +0200

    Formating output

--- university_pinger/pinger.py
+++ university_pinger/pinger.py
@@ -1,19 +1,32 @@
 import os
 import sys
 
+def print_lib(lib):
+    if len(lib) == 0:
+        print("No IPs to print")
+        return
+        
+    print("\n\nIPs up:\n")
+    for key in lib:
+        if lib[key] == "up":
+            print(f"{key}")
+
+
 def main():
-    if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} <filename>")
+    
+    if len(sys.argv) != 4:
+        print(f"Usage: {sys.argv[0]} <room> <first no.> <last no.>")
         sys.exit(1)
-    else:
-        room = sys.argv[1]
 
-    for i in range(20):
-        ip = f"{room}{i}.iem.pw.edu.pl";
-        response = os.system(f"ping {ip}")
-        if response == 0:
-            print(f"{ip} is up")
-        else:
-            print(f"{ip} is down")
+    room = sys.argv[1]
+    result = dict()
+    first, last = int(sys.argv[2]), int(sys.argv[3])
+    for i in range(first, last+1):
+        print(f"Pinging {room}{i:02}.iem.pw.edu.pl: ", end="", flush=True)
+        ip = f"{room}{i:02}.iem.pw.edu.pl"
+        response = os.system(f"ping -c 1 {ip} > /dev/null 2>&1")
+        result[ip] = "up" if response == 0 else "down"
+        print(result[ip])
 
+    print_lib(result)
 main()
