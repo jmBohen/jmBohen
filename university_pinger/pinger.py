73fa35d6e8c2c0605b3f04650eec5b554de08abc
jmBohen <jmbochenski@gmail.com>
2024-10-12 19:27:10 +0200

    Changed room cehcking algorythm

--- university_pinger/pinger.py
+++ university_pinger/pinger.py
@@ -14,19 +14,19 @@ def print_lib(lib):
 
 def main():
     
-    if len(sys.argv) != 4:
-        print(f"Usage: {sys.argv[0]} <room> <first no.> <last no.>")
+    if len(sys.argv) != 3:
+        print(f"Usage: {sys.argv[0]} <first no.> <last no.>")
         sys.exit(1)
 
-    room = sys.argv[1]
     result = dict()
-    first, last = int(sys.argv[2]), int(sys.argv[3])
+    first, last = int(sys.argv[1]), int(sys.argv[2])
     for i in range(first, last+1):
-        print(f"Pinging {room}{i:02}.iem.pw.edu.pl: ", end="", flush=True)
-        ip = f"{room}{i:02}.iem.pw.edu.pl"
-        response = os.system(f"ping -c 1 {ip} > /dev/null 2>&1")
-        result[ip] = "up" if response == 0 else "down"
-        print(result[ip])
+        for room in ["p", "l", "k", "gg"]:
+            print(f"Pinging {room}{i:02}.iem.pw.edu.pl: ", end="", flush=True)
+            ip = f"{room}{i:02}.iem.pw.edu.pl"
+            response = os.system(f"ping -c 1 {ip} > /dev/null 2>&1")
+            result[ip] = "up" if response == 0 else "down"
+            print(result[ip])
 
     print_lib(result)
 main()
